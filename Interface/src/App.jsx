85dd1f018759d655c7f678db349021d035bf9929
Kaique marques <kaiquemarques581@outlook.com>
2024-12-11 10:21:19 -0300

    Adicionando estrutura inicial da interface web

---
+++ Interface/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+//import productIcon from './assets/product.svg'
+import Home from './pages/home/Home';
+
+function App() {
+
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
